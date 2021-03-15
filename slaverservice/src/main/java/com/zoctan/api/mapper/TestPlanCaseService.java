0fa4bb7b5d776b695e857c9936d7fd9e2f72fa2c
Season <2712066939@qq.com>
2021-03-15 10:10:18 +0800

    机构图

---
+++ slaverservice/src/main/java/com/zoctan/api/mapper/TestPlanCaseService.java
@@ -0,0 +1,8 @@
+package com.zoctan.api.mapper;
+
+import com.zoctan.api.core.mapper.MyMapper;
+import com.zoctan.api.dto.TestplanCase;
+
+public interface TestPlanCaseService extends MyMapper<TestplanCase> {
+
+}
