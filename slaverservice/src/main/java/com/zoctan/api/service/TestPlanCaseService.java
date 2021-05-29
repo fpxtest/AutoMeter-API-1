1d358b83d7fddbac45c218665bd7ebf5f92b9097
Season <2712066939@qq.com>
2021-05-29 14:49:50 +0800

    testplancase

---
+++ slaverservice/src/main/java/com/zoctan/api/service/TestPlanCaseService.java
@@ -0,0 +1,18 @@
+package com.zoctan.api.service;
+
+import com.zoctan.api.core.service.Service;
+import com.zoctan.api.dto.TestplanCase;
+
+/**
+* @author Zoctan
+* @date 2020/04/17
+*/
+public interface TestPlanCaseService extends Service<TestplanCase> {
+
+    void ExecuteHttpPerformancePlanCase(String CaseType,long SlaverId,long BatchId,long PlanId,long CaseId,Long Thread,Long Loop,String DeployName,String JmeterPath,String JmxPath,String JmxCaseName,String BatchName,String JmeterPerformanceReportPath,String MysqlUrl,String MysqlUserName,String MysqlPassword);
+
+    void ExecuteHttpPlanFunctionCase(String JmeterPath,String JmxPath,String DispatchIds,String MysqlUrl,String MysqlUsername,String MysqlPassword);
+
+
+
+}
