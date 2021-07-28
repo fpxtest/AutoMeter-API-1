db7652b6668b10b3f8783018f9ceb77ccf6e941b
season <season@192.168.0.101>
2021-07-28 21:05:32 +0800

    clean

--- dispatchservice/resetDB.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in $(find /Users/fanseasn/Work/QAP/spring-boot-vue-admin/api/src/test/resources/sql/dev/*.sql) ; do
-  /usr/local/MySQL/bin/mysql -u root -p 123456 admin_test < ${i};
-done
