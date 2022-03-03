01a9fa2b80fcb6a47f5a7eec0f50cf5070d4b9f6
season <season@seasondeMacBook-Air.local>
2022-03-03 14:40:43 +0800

    bugfix

--- slaverservice/src/main/java/com/zoctan/api/mapper/TestvariablesValueMapper.java
+++ slaverservice/src/main/java/com/zoctan/api/mapper/TestvariablesValueMapper.java
@@ -2,6 +2,7 @@ package com.zoctan.api.mapper;
 
 import com.zoctan.api.core.mapper.MyMapper;
 import com.zoctan.api.entity.TestvariablesValue;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.entity.Condition;
 
 import java.util.List;
@@ -22,6 +23,6 @@ public interface TestvariablesValueMapper extends MyMapper<TestvariablesValue> {
 
     int ifexist(Condition condition);
 
-    List<TestvariablesValue> gettvlist(Long planid,String batchname);
+    List<TestvariablesValue> gettvlist(@Param("planid") Long planid, @Param("batchname")String batchname);
 
 }
