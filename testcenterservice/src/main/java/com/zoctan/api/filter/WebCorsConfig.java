6e3111dd300d21d9c730b18475cf5cfc4d8c59b4
JWang <yydm0326@outlook.com>
2022-03-17 00:09:38 +0800

    新增testcenterservice简单跨域处理 新增docker-compose启动方式

---
+++ testcenterservice/src/main/java/com/zoctan/api/filter/WebCorsConfig.java
@@ -0,0 +1,18 @@
+package com.zoctan.api.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebCorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
