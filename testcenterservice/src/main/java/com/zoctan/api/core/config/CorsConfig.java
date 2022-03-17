3d11668d4cc9f97e270b30430d935555eb460f57
= <yydm19950326>
2022-03-17 10:35:26 +0800

    重新处理跨域，

--- testcenterservice/src/main/java/com/zoctan/api/core/config/CorsConfig.java
+++ testcenterservice/src/main/java/com/zoctan/api/core/config/CorsConfig.java
@@ -1,11 +1,8 @@
 package com.zoctan.api.core.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.util.pattern.PathPatternParser;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 处理跨域，使用通配符
@@ -14,22 +11,14 @@ import org.springframework.web.util.pattern.PathPatternParser;
  * @date 2022/3/17 9:06
  */
 @Configuration
-public class CorsConfig {
+public class CorsConfig extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public CorsWebFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-        source.registerCorsConfiguration("/**", buildConfig());
-        return new CorsWebFilter(source);
-    }
-
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //在生产环境上最好指定域名，以免产生跨域安全问题
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        return corsConfiguration;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 }
