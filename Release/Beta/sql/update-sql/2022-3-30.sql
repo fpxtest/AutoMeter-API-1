05208b6d9f10522ab5a8a2025aa855f6bdac4d2f
season <season@seasondeMacBook-Air.local>
2022-03-30 12:26:21 +0800

    sql-update

---
+++ Release/Beta/sql/update-sql/2022-3-30.sql
@@ -0,0 +1,4 @@
+ALTER TABLE testcenter.apicases_variables ADD COLUMN `apiid` bigint(20) unsigned NOT NULL COMMENT 'apiid';
+
+ALTER TABLE testcenter.apicases_variables ADD COLUMN `deployunitid` bigint(20) unsigned NOT NULL COMMENT 'deployunitid';
+
