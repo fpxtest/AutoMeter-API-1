da86c596ebc9ae5bc545242044b56dd3d8ffaa53
season <season@seasondeMacBook-Air.local>
2022-03-30 19:44:39 +0800

    sql-update

--- Release/Beta/sql/update-sql/2022-3-30.sql
+++ Release/Beta/sql/update-sql/2022-3-30.sql
@@ -2,3 +2,6 @@ ALTER TABLE testcenter.apicases_variables ADD COLUMN `apiid` bigint(20) unsigned
 
 ALTER TABLE testcenter.apicases_variables ADD COLUMN `deployunitid` bigint(20) unsigned NOT NULL COMMENT 'deployunitid';
 
+ALTER TABLE testcenter.dictionary MODIFY COLUMN dicitmevalue varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '字典项值';
+
+
