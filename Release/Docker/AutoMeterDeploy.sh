04e630a6f5aa497ca517b62cabf2d57d0c774aa4
season <season@seasondeMacBook-Air.local>
2022-04-21 18:03:06 +0800

    docker-compose部署优化

--- Release/Docker/AutoMeterDeploy.sh
+++ Release/Docker/AutoMeterDeploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 echo $ip
-sed -i ”“ "s@192.168.3.95@${ip}@" ../Beta/conditionservice/config/application.yml 
-sed -i ”“ "s@192.168.3.95@${ip}@" ../Beta/dispatchservice/config/application.yml 
-sed -i ”“ "s@192.168.3.95@${ip}@" ../Beta/slaverservice/config/application.yml 
-sed -i ”“ "s@192.168.3.95@${ip}@" ../Beta/testcenterservice/config/application.yml 
-sed -i ”“ "s@192.168.3.95@${ip}@" ../Beta/testcenterapp/dist/static/config.js
+sed -i ”“ "s@127.0.0.1@${ip}@" ../Beta/conditionservice/config/application.yml 
+sed -i ”“ "s@127.0.0.1@${ip}@" ../Beta/dispatchservice/config/application.yml 
+sed -i ”“ "s@127.0.0.1@${ip}@" ../Beta/slaverservice/config/application.yml 
+sed -i ”“ "s@127.0.0.1@${ip}@" ../Beta/testcenterservice/config/application.yml 
+sed -i ”“ "s@127.0.0.1@${ip}@" ../Beta/testcenterapp/dist/static/config.js
+echo "修改IP成功"
 docker-compose -f docker-compose.yaml up -d 
 echo "AutoMeter部署成功，执行完增量sql后，访问入口 http://$ip:8084  默认账户密码admin admin123"
